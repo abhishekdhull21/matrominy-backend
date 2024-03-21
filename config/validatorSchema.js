ac8ece92aa0823459df98d80d17b7d803f39073e
abhishekdhull <46604435+abhishekdhull21@users.noreply.github.com>
2024-03-21 12:31:08 +0530

    change validation condition

--- config/validatorSchema.js
+++ config/validatorSchema.js
@@ -3,14 +3,12 @@ const Joi = require('joi');
 module.exports.userValidSchema = Joi.object({
         username: Joi.string()
                 .min(3)
-                .max(30)
-                .required(),
+                .max(30),
         password: Joi.string()
                 .min(6)
                 .required(),
         email:  Joi.string()
-                .email()
-                .required(),        
+                .email(),
         mobile: Joi.string()
                 .pattern(/^[789]\d{9}$/) // Indian mobile numbers start with 7, 8, or 9 and have a total of 10 digits
                 .message('Please enter a valid mobile number').required(),
